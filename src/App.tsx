73ac622736b0ac8012bbfbe53cfab32d46d8e776
ChillWoW <97155839+ChillWoW@users.noreply.github.com>
2025-11-05 20:47:24 +0200

    Remove old files

--- src/App.tsx
+++
@@ -1,9 +0,0 @@
-const App: React.FC = () => {
-    return (
-        <div className="flex min-h-screen bg-dark-900 items-center justify-center text-white">
-            Vite is working!
-        </div>
-    );
-};
-
-export default App;
